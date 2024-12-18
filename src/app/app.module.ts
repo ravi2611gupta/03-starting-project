ce7dcc50463372c4d4476177396ab39ea5fd64b3
ravi <rvi2611gupta>
2024-12-18 17:28:15 +0530

    70 video completed and migrated the whole app to the module based

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
-import { NgModule } from "@angular/core";
-import { AppComponent } from "./app.component";
-import { HeaderComponent } from "./header/header.component";
-import { UserComponent } from "./user/user.component";
-import { TasksComponent } from "./tasks/tasks.component";
-import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+// import { DatePipe } from "@angular/common";
+import { FormsModule } from '@angular/forms';
+
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { CardComponent } from './shared/card/card.component';
+import { NewTaskComponent } from './tasks/new-task/new-task.component';
+import { TaskComponent } from './tasks/task/task.component';
+
 
 @NgModule({
-    declarations: [AppComponent],
-    bootstrap: [AppComponent],
-    imports: [BrowserModule, HeaderComponent, UserComponent, TasksComponent]
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    UserComponent,
+    CardComponent,
+    TasksComponent,
+    TaskComponent,
+    NewTaskComponent,
+  ],
+  bootstrap: [AppComponent],
+  imports: [
+    BrowserModule,
+    //  DatePipe,
+    FormsModule,
+  ],
 })
-// ! BrowserModule used to handle this error: A required Injectable was not found in the dependency injection tree. If you are bootstrapping an NgModule, make sure that the `BrowserModule` is imported.
-export class AppModule {
 
-}
+// ! BrowserModule used to handle this error: A required Injectable was not found in the dependency injection tree. If you are bootstrapping an NgModule, make sure that the `BrowserModule` is imported.
+
+// ! All the pipes module by angular comes with the BrowserModule so we don't need to add DatePipe heres
+export class AppModule {}
--- src/app/shared/card/card.component.ts
+++ src/app/shared/card/card.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-card',
-  standalone: true,
-  imports: [],
+  // standalone: true,
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
